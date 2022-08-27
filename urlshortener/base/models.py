95aa8ebd15434b27caf02c351c2563103a3954e4
mutlu <mutlucan.tokat@akinon.com>
2022-08-27 13:01:44 +0300

    Add url and hit models

--- urlshortener/base/models.py
+++ urlshortener/base/models.py
@@ -1,3 +1,17 @@
+from typing import List
+
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, update_fields: List = None, **kwargs):
+        if update_fields is not None:
+            update_fields.append("updated_at")
+            kwargs["update_fields"] = update_fields
+        super(BaseModel, self).save(**kwargs)
--- urlshortener/urls/models.py
+++ urlshortener/urls/models.py
@@ -1,3 +1,35 @@
+from base.models import BaseModel
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Url(BaseModel):
+    long_url = models.CharField(max_length=512)
+    key = models.CharField(max_length=7, unique=True)
+    user_id = (
+        models.PositiveIntegerField()
+    )  # Probably will come from user service or auth service
+
+    class Meta:
+        verbose_name = _("url")
+        verbose_name_plural = _("urls")
+        indexes = [
+            models.Index(
+                fields=[
+                    "long_url",
+                ]
+            ),
+            models.Index(
+                fields=[
+                    "short_url",
+                ]
+            ),
+            models.Index(
+                fields=[
+                    "user_id",
+                ]
+            ),
+        ]
+
+    def __str__(self) -> str:
+        return self.key

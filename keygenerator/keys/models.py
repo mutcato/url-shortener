799cbd3fdf659b1e770230d0844d376eea1fa61c
mutlu <mutlucan.tokat@akinon.com>
2022-08-29 23:14:56 +0300

    Create keygenerator project and keys app

---
+++ keygenerator/keys/models.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from django.db import models
+
+
+class Key(models.Model):
+    key = models.CharField(max_length=7)
+    is_used = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
